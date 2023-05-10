d92261cd5624e1851ba3f22177b694594f9177ba
veryverygreen <108616726+veryverygreen@users.noreply.github.com>
2023-05-10 21:56:57 +0500

    Update search with firefox.py

--- firefox/search with firefox.py
+++ firefox/search with firefox.py
@@ -1,8 +1,6 @@
-"""driver.execute_script("window.history.go(-1)") Не работает в firefox!!!"""
-
 from functions import *
 
-with open("C:\\Users\\Алексей\\PycharmProjects\\automation_WEB_task_2\\firefox\\links.txt", "w") as file:
+with open("path_to_links.txt", "w") as file:
     file.write("")
 
 def main():
